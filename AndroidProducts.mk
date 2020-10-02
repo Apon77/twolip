937ce0f8604c9899b93133b1b2444dc23b70d806
Khalakuzzaman Apon <khalakuzzamanapon5@gmail.com>
2020-10-03 01:27:17 +0600

    twolip: SuperiorOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_twolip.mk
+    $(LOCAL_DIR)/superior_twolip.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_twolip-user \
-    lineage_twolip-userdebug \
-    lineage_twolip-eng
+    superior_twolip-user \
+    superior_twolip-userdebug \
+    superior_twolip-eng
